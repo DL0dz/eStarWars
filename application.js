afeaffe3ea5f36ba0c355913c223aaf571fe2201
Loic Laudet <loic.laudet@yahoo.fr>
2015-12-29 10:58:08 +0100

    removing home.js routing

--- application.js
+++ application.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 require('./app/models/connection');
 
-app.use('/', require('./app/controllers/home'));
 app.use('/', require('./app/controllers/product'));
 
 
