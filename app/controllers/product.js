095c403062ae7ebe16a1c4521d3b4016b2d6d325
Loic Laudet <loic.laudet@yahoo.fr>
2015-12-29 10:38:12 +0100

    get product routing and method creation

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -2,7 +2,19 @@ const debug = require('debug')('estarwars:controllers:product');
 const express = require('express');
 const router = module.exports = new express.Router();
 const Product = require('../models/product');
+const categories = ['lasers', 'helmets'];
 
+function showProducts(req, res) {
+  const path = req.route.path;
+  const category = path.slice(1, path.length);
+
+  Product.retrieveProducts(category)
+    .then(function callback(products) {
+      res.send(products);
+    }, function error(err) {
+      debug('error : ', err);
+    });
+}
 
 function createProduct(req, res) {
   const starWarsProduct = new Product({
@@ -12,7 +24,7 @@ function createProduct(req, res) {
     published: false, // req.body.published
     quantity: 30, // req.body.quantity
     price: 499, // req.body.price
-    category: 'laser', // req.body.category
+    category: 'lasers', // req.body.category
     tags: ['jedi', 'alliance'], // req.body.tags
   });
 
@@ -28,7 +40,6 @@ function createProduct(req, res) {
 function updateProduct(req, res) {
   const productId = '568161139668aa6a35dc5c10'; // req.params.id
   const productUpdatedInfos = {'category': 'cap', 'price': 349}; // req.body
-  debug('req.route :  : ', req.route.path);
 
   Product.modifyProduct(productId, productUpdatedInfos)
     .then(function callback(productUpdated) {
@@ -40,6 +51,11 @@ function updateProduct(req, res) {
 }
 
 // ## Routing table
+categories.forEach(function(category) {
+  router.route('/' + category)
+    .get(showProducts);
+});
+
 router.route('/api/products')
   .post(createProduct);
 
