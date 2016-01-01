ad842c8ca054d8d9ac742091967472b3f94fa701
Loic Laudet <loic.laudet@yahoo.fr>
2016-01-02 00:21:12 +0100

    post user signup routing and method creation

---
+++ app/controllers/user.js
@@ -0,0 +1,35 @@
+const debug = require('debug')('estarwars:controllers:user');
+const express = require('express');
+const router = module.exports = new express.Router();
+const User = require('../models/user');
+
+function createUser(req, res) {
+  const newUser = new User({
+    firstname: 'Jean',
+    lastname: 'Jeannot',
+    email: 'jean@jeannot.com',
+    admin: false,
+    address: {
+      street: '6 rue Souchal',
+      zip: '92110',
+      city: 'Clichy',
+      state: 'France',
+    },
+    cart: [null],
+  });
+
+  const hash = User.generateHash('force'); // req.body.password
+  newUser.password = hash;
+
+  User.saveUser(newUser)
+    .then(function callback(userSaved) {
+      debug('userSaved : ', userSaved);
+      res.send(userSaved);
+    }, function error(err) {
+      debug('error : ', err);
+      res.send(err);
+    });
+}
+
+router.route('/signup')
+  .post(createUser);
