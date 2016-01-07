f66acc8c1877bb31690e193f3a2baa3f2d00021e
Pierrick TURELIER <pierrick@oxynum.fr>
2016-01-07 23:33:26 +0100

    Edit Product Page added

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -4,9 +4,12 @@
   console.log('hello world !');
 })();
 
+// Dashboard Page
+
+// Button to remove a product
 (function() {
   function deleteProduct(event) {
-    const url = this.getAttribute('href');
+    var url = this.getAttribute('href');
 
     event.preventDefault();
 
@@ -24,13 +27,14 @@
   $('.btn-danger').on('click', deleteProduct);
 })();
 
+// Switch button to change the status of a product
 (function() {
   $("[name='status-checkbox']").bootstrapSwitch();
 
   $("[name='status-checkbox']").on('switchChange.bootstrapSwitch', function(event, state) {
-    const status = this.dataset.published;
-    const id = this.dataset.id;
-    const url = '/api/products/' + id;
+    var status = this.dataset.published;
+    var id = this.dataset.id;
+    var url = '/api/products/' + id;
 
     function changeStatus(event, status, url, id) {
       event.preventDefault();
@@ -56,4 +60,48 @@
 
     changeStatus(event, status, url, id);
   });
+})();
+
+// Edit Page
+(function() {
+  function editProduct(event) {
+    event.preventDefault();
+
+    var url = this.getAttribute('href');
+    var form = $('#edit-form')[0];
+    var dataElement = {};
+
+    for(var i=0; i < form.elements.length; i++){
+      if (form.elements[i].getAttribute('type') == 'radio' && !(form.elements[i].checked)) {
+        form.elements[i].value = '';
+        dataElement[i] = form.elements[i].value;
+      } else if (form.elements[i].getAttribute('type') == 'checkbox' && !(form.elements[i].checked)) {
+        form.elements[i].value = '';
+        dataElement[i] = form.elements[i].value;
+      } else if (form.elements[i] === '') {
+        return;
+      }
+      dataElement[i] = form.elements[i];
+    }
+
+    var data = {
+      title: dataElement[0].value,
+      content: dataElement[1].value, 
+      quantity: dataElement[2].value, 
+      price: dataElement[3].value, 
+      category: dataElement[4].value || dataElement[5].value, 
+      tags: [dataElement[6].value, dataElement[7].value],
+    };
+
+    $.ajax({
+      url: url,
+      type: 'PUT',
+      data: data,
+      success: function (data) {
+        window.location.href = '/dashboard';
+      }
+    });
+  }
+
+  $('.btn-edit').on('click', editProduct);
 })();
