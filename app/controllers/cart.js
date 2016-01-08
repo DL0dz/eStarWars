4dc8b3751a41cd74da12ca48dd46ed892aa5e8ad
Loic Laudet <loic.laudet@yahoo.fr>
2016-01-08 21:52:24 +0100

    removing unused commented code

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -2,21 +2,6 @@ const debug = require('debug')('estarwars:controllers:cart');
 const express = require('express');
 const router = module.exports = new express.Router();
 const User = require('../models/user');
-// const Product = require('../models/product');
-
-// function showCart(req, res) {
-//   const userId = req.params.id;
-//   if (userId) {
-//     User.retrieveCart(userId)
-//     .then(function callback(products) {
-//       res.render('cart', {user: req.user, products: products});
-//     }, function error(err) {
-//       debug('error : ', err);
-//     });
-//   } else {
-//     // get localstorage products
-//   }
-// }
 
 function showUserCart(req, res) {
   if (req.user) {
